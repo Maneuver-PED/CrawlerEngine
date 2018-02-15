e84fd8b208b0846b4ed93703079a0880b1c8359a
Gabriel Iuhasz <juhasz_gabriel@yahoo.com>
2018-02-15 14:22:46 +0200

    added util

---
+++ crawl_engine/utils/eq.py
@@ -0,0 +1,45 @@
+eq_aws = {
+    "us-east-2":	"Ohio",
+    "us-east-1":	"NVirginia",
+    "us-west-1":	"Northern_California",
+    "us-west-2":	"Oregon",
+    "ap-south-1":	"Mumbai",
+    "ap-northeast-2":	"Seoul",
+    "ap-southeast-1": "Singapore",
+    "ap-southeast-2":	"Sydney",
+    "ap-northeast-1":	"Tokyo",
+    "ca-central-1":	"Canada",
+    "cn-north-1":	"Beijing",
+    "eu-central-1":	"Frankfurt",
+    "eu-west-1":	"Ireland",
+    "eu-west-2":	"London",
+    "eu-west-3":	"Paris",
+    "sa-east-1":	"Sao_Paulo"
+}
+
+eq_google = {
+      # "us": 4.0240,
+      "us-central1": "Iowa",
+      "us-east1": "SCarolina",
+      "us-east4": "NVirginia",
+      "us-west1": "Oregon",
+      # "europe": 4.0240,
+      "europe-west1": "Belgium",
+      "europe-west2": "London",
+      "europe-west3": "Frankfurt",
+      "europe-west4": "Netherlands",
+      "northamerica-northeast1": "Canada",  # Montreal
+      # "asia": 4.0240,
+      "asia-east": "Taiwan",
+      "asia-northeast": "Tokyo",
+      "asia-southeast": "Singapore",
+      "australia-southeast1": "Sidney",
+      # "australia": 5.4324,
+      "southamerica-east1": "Sao_Paulo",
+      "asia-south1": "Mumbai"
+}
+
+eq_OS = {
+    "linux": "Linux",
+
+}
